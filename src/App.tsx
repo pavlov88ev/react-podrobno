ceb204dd5f1bacbda37d284e6b6e7a8a30ca9151
Eugene Pavlov <pavlov88ev@gmail.com>
2023-07-06 00:15:16 +0300

    Sprint 01 week 3

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 import Accordion from "./components/Accordion/Accordion";
 import {Rating} from "./components/Rating/Rating";
+import OnOff from "./components/OnOff/OnOff";
+import UncontrolledAccordion from "./components/UncontrolledAccordion/UncontrolledAccordion";
+import {UncontrolledRating} from "./components/UncontrolledRating/UncontrolledRating";
 
 
 function App() {
@@ -9,20 +12,31 @@ function App() {
     // обязана вернуть JSX
     console.log("App rendering")
     return (
-        <div>
+        <div className={'App'}>
+            <OnOff/>
+
+            <UncontrolledAccordion titleValue={"Menu"}/>
+            <Accordion titleValue={"Menu"} collapsed={false}/>
+
+            <UncontrolledRating/>
+            <Rating value={3}/>
+
+            {/*<OnOff on={false}/>
+            <OnOff on={true}/>*/}
+
             {/*<PageTitle title={"This is App component"}/>
             <PageTitle title={"My friends"}/>
             Article 1
             <Rating value={3}/>*/}
-            <Accordion titleValue={"Menu"} collapsed={true}/>
+            {/*<Accordion titleValue={"Menu"} collapsed={true}/>
             <Accordion titleValue={"Users"} collapsed={false}/>
-            {/*Article 2*/}
+            Article 2
             <Rating value={0}/>
             <Rating value={1}/>
             <Rating value={2}/>
             <Rating value={3}/>
             <Rating value={4}/>
-            <Rating value={5}/>
+            <Rating value={5}/>*/}
         </div>
     );
 }
--- src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
+++ src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 type AccordionPropsType = {
-    titleValue: string,
-    collapsed: boolean
+    titleValue: string
 }
 
-function Accordion(props: AccordionPropsType) {
+function UncontrolledAccordion(props: AccordionPropsType) {
+
+    const [collapsed, setCollapsed] = useState(false)
+
     console.log("UncontrolledAccordion rendering")
+
     return (
         <div>
-            <AccordionTitle title={props.titleValue}/>
-            { !props.collapsed && <AccordionBody/>}
+            <AccordionTitle title={props.titleValue}/><button onClick={ () => setCollapsed(!collapsed) }>TOGGLE</button>
+            { !collapsed && <AccordionBody/>}
         </div>
     );
 }
@@ -35,4 +38,4 @@ function AccordionBody() {
     );
 }
 
-export default Accordion;
+export default UncontrolledAccordion;
